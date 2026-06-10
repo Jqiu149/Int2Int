a812c237a5ac3a83115367d54533341166f70b23
Jason Qiu <jasonqiu93.com>
2026-06-10 17:48:03 +0000

    main thing is we realized we read pseudocode wrong for Lagrange reduce. and we should be more caereful with new notation and not assume what it is... i mean we also just hsouldn't have used floor because that wasn't what we thoguht it was either, but it should be roudn to the closest integer not towards zero either. and also replaced how we were computing squared norm with dot prodcuts for no real reason...

--- src/envs/generators.py
+++ src/envs/generators.py
@@ -206,8 +206,8 @@ def LagrangeReduce(v1,v2):
     v1 = np.array(v1)
     v2 = np.array(v2)
 
-    norm1Squared = v1[0]**2 + v1[1]**2
-    norm2Squared = v2[0]**2 + v2[1]**2
+    norm1Squared = np.dot(v1, v1) 
+    norm2Squared = np.dot(v2,v2) 
 
     done = False
     while(not done):
@@ -215,9 +215,9 @@ def LagrangeReduce(v1,v2):
             v1,v2 = v2,v1
             norm1Squared,norm2Squared = norm2Squared,norm1Squared
 
-        u = math.floor( (np.dot(v1,v2))/ norm1Squared)
+        u = round( (np.dot(v1,v2))/ norm1Squared)
         v2 = v2-u*v1
-        norm2Squared= v2[0]**2 + v2[1]**2
+        norm2Squared= np.dot(v2,v2) 
 
         if(norm1Squared<= norm2Squared):
             done = True
